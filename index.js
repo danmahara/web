8eb580464c4f838268d08650b0cf85e8a03bef8b
Dan Mahara <dan.mahara.0909@gmail.com>
2023-12-20 12:04:36 +0545

    changes

---
+++ index.js
@@ -0,0 +1,39 @@
+
+    let texts = ["Front-end developer", "Full-stack developer"];
+    let currentIndex = 0;
+    let currentCharacterIndex = 0;
+    let isDeleting = false;
+    let timeOutId;
+
+    function changeText() {
+        let textValue = document.getElementById('textChange');
+        let currentText = texts[currentIndex];
+        if (!isDeleting && currentCharacterIndex < currentText.length) {
+
+            textValue.textContent = currentText.substring(0, currentCharacterIndex + 1);
+            currentCharacterIndex++;
+
+
+
+        }
+        else {
+            isDeleting = true;
+
+            if (currentCharacterIndex > 0) {
+                timeOutId = setTimeout(() => {
+                    textValue.textContent = currentText.substring(0, currentCharacterIndex - 1);
+                    currentCharacterIndex--;
+                }, 1000);
+            } else {
+                clearTimeout(timeOutId);
+                isDeleting = false;
+                // Move to the next text
+                currentIndex = (currentIndex + 1) % texts.length;
+
+            }
+
+        }
+    }
+
+
+    setInterval(changeText, 100);
